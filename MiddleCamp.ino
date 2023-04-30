c8508cc2a28a0f630f2cf2471e0fd32281de064a
Nicolas Hostert <nicolas@hostert.ca>
2023-04-30 15:39:39 -0400

    Added Breathing mode

--- MiddleCamp.ino
+++ MiddleCamp.ino
@@ -1,73 +1,63 @@
 #include <Adafruit_NeoPixel.h>
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
-#define TESTPIN            6
-
-// How many NeoPixels are attached to the Arduino?
-#define TESTNUMPIXELS      15
-
+// DEFINE AND VAR ALL THE THINGS
+#define TESTPIN            6 // datapin for the strip
+#define TESTNUMPIXELS      15 // Number of LEDs
+#define SLIDERDELAY  50 // Delay in sliderfull and sliderblock modes 
+#define TESTDELAY 150   // Delay for the init script
 Adafruit_NeoPixel teststrip = Adafruit_NeoPixel(TESTNUMPIXELS, TESTPIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
+  // To confirm the strip is working as planned. Goes Off, Red, Green, Blue, Off (+2s pause).
   teststrip.begin();
-  // black
   blackout();
-  // red
   for (int i = 0; i < teststrip.numPixels(); i = i + 1) {
     teststrip.setPixelColor(i, 255, 0, 0);    //turn every pixel off
   }
   teststrip.show();
-  delay(150);
-  // green
+  delay(TESTDELAY);
   for (int i = 0; i < teststrip.numPixels(); i = i + 1) {
     teststrip.setPixelColor(i, 0, 255, 0);    //turn every pixel off
   }
   teststrip.show();
-  delay(150);
-  // blue
+  delay(TESTDELAY);
   for (int i = 0; i < teststrip.numPixels(); i = i + 1) {
     teststrip.setPixelColor(i, 0, 0, 255);    //turn every pixel off
   }
   teststrip.show();
-  delay(150);
-  // black
+  delay(TESTDELAY);
   blackout();
   delay(2000);
 }
 
 void sliderfull(int color1, int color2, int color3) {
-  int pause = 50;
-  // Off first
+  // Fills the strip and drains it afterwards, always same direction
   blackout();
   for (int i = 0; i < 99; i = i + 1) {
     teststrip.setPixelColor((i*teststrip.numPixels()/100), color1, color2, color3);
     teststrip.show();
-    delay(pause);
+    delay(SLIDERDELAY);
   }
   for (int i = 0; i < 99; i = i + 1) {
     teststrip.setPixelColor((i*teststrip.numPixels()/100), 0, 0, 0);
     teststrip.show();
-    delay(pause);
+    delay(SLIDERDELAY);
   }
 }
 
-void sliderblock(int color1, int color2, int color3) {
-  int pause = 50;
-  int blocksize = 4;
-
-  // Off first
+void sliderblock(int color1, int color2, int color3, int blocksize) {
+  // Slides a block of LEDs across
   blackout();
   for (int i = 0; i < 99; i = i + 1) {
     teststrip.setPixelColor((i*teststrip.numPixels()/100), color1, color2, color3);
     teststrip.setPixelColor(((i*teststrip.numPixels()/100)-blocksize), 0, 0, 0);
     teststrip.show();
-    delay(pause);
+    delay(SLIDERDELAY);
   }
 }
 
 void blackout() {
+  // Turns the strip all back
   for (int i = 0; i < teststrip.numPixels(); i = i + 1) {
     teststrip.setPixelColor(i, 0, 0, 0);    //turn every pixel off
   }
@@ -75,7 +65,27 @@ void blackout() {
 
 }
 
+void breathing(int color1, int color2, int color3, int delayvalue) {
+  // Goes from 0 to 255 power on the specified colors, back to 0
+  for (int level = 0; level < 255; level = level + 1) {
+    for (int i = 0; i < 99; i = i + 1) {
+      teststrip.setPixelColor((i*teststrip.numPixels()/100), (level*color1/255) , (level*color2/255), (level*color3/255));
+    }
+    teststrip.show();
+    delay(delayvalue);
+  }
+   for (int level = 255; level > 0; level = level - 1) {
+    for (int i = 0; i < 99; i = i + 1) {
+      teststrip.setPixelColor((i*teststrip.numPixels()/100), (level*color1/255) , (level*color2/255), (level*color3/255));
+      }
+    teststrip.show();
+    delay(delayvalue);
+  }
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-  sliderblock(0,0,255);
+  // sliderfull(0,0,255);
+  // sliderblock(0,0,255,3);
+  breathing(0,0,255,5);
 }
